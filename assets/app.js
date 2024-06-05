abb82ac2612af3752ee386c6093a952ab0c5b862
llayz46 <layzer@outlook.fr>
2024-06-05 17:35:55 +0200

    adding user login, logout, register, and start to create list

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,8 @@ import './bootstrap.js';
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * which should already be in your baseAdmin.html.twig.
  */
-import './styles/app.css';
+import './styles/app.sass';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
